02e157fcef6896ac58171611247ceb6385a7539b
costudiolabor <74557729+costudiolabor@users.noreply.github.com>
2021-03-17 00:08:38 +0300

    Add pointSpawn pointFinish create GameManager

---
+++ Project_Platformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabPlayer;
+    [SerializeField]
+    private Transform pointSpawn;
+    [SerializeField]
+    private Transform pointFinish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
